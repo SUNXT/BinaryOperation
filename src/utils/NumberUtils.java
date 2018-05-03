c03ce6eeab5265946c6f2f4aaaffcce97eb03a3d
sunxuedian <sunxt16@qq.com>
2018-05-04 01:31:29 +0800

    update

---
+++ src/utils/NumberUtils.java
@@ -0,0 +1,81 @@
+package utils;
+
+import com.sun.org.apache.regexp.internal.RE;
+
+import java.util.regex.Pattern;
+
+public class NumberUtils {
+
+    /**
+     * 判断是否为整数
+     * @param str
+     * @return 是整数返回true,否则返回false
+     */
+    public static boolean isInteger(String str) {
+        if (str == null || str.equals("")){
+            return false;
+        }
+        Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$");
+        return pattern.matcher(str).matches();
+    }
+
+    /**
+     * 十进制转二进制原码，带符号
+     * @param bitLength 位数
+     * @param dText
+     * @return
+     */
+    public static String transBinNum(int bitLength, String dText){
+        int num = Integer.parseInt(dText);
+        String binNum = Integer.toBinaryString(Math.abs(num));
+        int zeroLength = bitLength - binNum.length();//需要补零的位数
+        String zeroStr = "";
+        for (int i = 0; i < zeroLength; ++ i){
+            zeroStr += "0";
+        }
+
+        if (num > 0 ){
+            return "0" + zeroStr + binNum;
+        }
+        return "1" + zeroStr + binNum;
+    }
+
+    /**
+     * 十进制转二进制补码，带符号
+     * @param bitLength
+     * @param dText
+     * @return
+     */
+    public static String transComplementNum(int bitLength, String dText){
+        int num = Integer.parseInt(dText);
+        if (num > 0){
+            return transBinNum(bitLength, dText);
+        }
+
+        String binNum = transBinNum(bitLength, dText);
+
+        //为负数，需要转换，从右往左扫描，遇到第一位1后的字节，进行反码处理
+        int rightOneIndex = binNum.lastIndexOf("1");//找从右边数过来第一个1 的位置
+        Log.d("the right one index is " + rightOneIndex);
+
+        int[] values = new int[binNum.length()];
+        for (int i = 0; i < values.length; ++ i ){
+            values[i] = binNum.charAt(i)-48;
+        }
+        int i = rightOneIndex - 1;
+        while (i > 0){
+            if (values[i] == 1){
+                values[i] = 0;
+            }else {
+                values[i] = 1;
+            }
+            i--;
+        }
+        StringBuilder newText = new StringBuilder();
+        for (int v : values){
+            newText.append(v);
+        }
+        return newText.toString();
+    }
+
+}
--- src/view/controller/MainViewController.java
+++ src/view/controller/MainViewController.java
@@ -1,11 +1,21 @@
 package view.controller;
 
+import binary.BinaryNum;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.InputMethodEvent;
 import utils.Log;
+import utils.NumberUtils;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,13 +24,87 @@ public class MainViewController implements Initializable {
 
     @FXML
     private Button mButton;
+    @FXML
+    private ComboBox<String> mComboBoxBit;//位数
+    @FXML
+    private ComboBox<String> mComboBoxType;//计算的数的类型
+    @FXML
+    private ComboBox<String> mCBoxCalculateNumType;//计算的数 原码、补码
+    @FXML
+    private ComboBox<String> mCBoxCalculateType;//选择 一位、二位
+
+    @FXML
+    private TextField mEditNum1,mEditNum2;
+    @FXML
+    private Label mLabelONum1, mLabelONum2, mLabelCNum1, mLabelCNum2;
+
+    private ObservableList<String> mComboBoxBitData;
+    private ObservableList<String> mComboBoxTypeData;
+    private ObservableList<String> mCBoxCalculateNumTypeData;
+    private ObservableList<String> mCBoxCalculateTypeData;
+
+    private int[] mBitLengthArys = {BinaryNum.TYPE_8_BIT, BinaryNum.TYPE_16_BIT};
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        initComboBox();
+        initTextField();
+    }
+
+    /**
+     * 初始化选择框
+     */
+    private void initComboBox(){
+        mComboBoxBitData = FXCollections.observableArrayList("八位/8 bit", "十六位/16 bit");
+        mComboBoxBit.setItems(mComboBoxBitData);
+        mComboBoxBit.setValue(mComboBoxBitData.get(0));
+
+        mComboBoxTypeData = FXCollections.observableArrayList("整数", "纯小数");
+        mComboBoxType.setItems(mComboBoxTypeData);
+        mComboBoxType.setValue(mComboBoxTypeData.get(0));
+
+        mCBoxCalculateNumTypeData = FXCollections.observableArrayList("原码运算", "补码运算");
+        mCBoxCalculateNumType.setItems(mCBoxCalculateNumTypeData);
+        mCBoxCalculateNumType.setValue(mCBoxCalculateNumTypeData.get(0));
+
+        mCBoxCalculateTypeData = FXCollections.observableArrayList("一位运算", "二位运算");
+        mCBoxCalculateType.setItems(mCBoxCalculateTypeData);
+        mCBoxCalculateType.setValue(mCBoxCalculateTypeData.get(0));
+    }
+
+    private void initTextField(){
+
+        mEditNum1.setText("0");
+        mEditNum1.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (NumberUtils.isInteger(newValue)){
+                int bitLength = mBitLengthArys[mComboBoxBitData.indexOf(mComboBoxBit.getValue())];
+                int num = Integer.parseInt(newValue);
+                mEditNum1.setText("" + num);
+                mLabelCNum1.setText(NumberUtils.transBinNum(bitLength, newValue));
+                mLabelONum1.setText(NumberUtils.transComplementNum(bitLength, newValue));
+            }else {
+                mEditNum1.setText(oldValue);
+            }
+
+        });
+
+        mEditNum2.setText("0");
+        mEditNum2.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (NumberUtils.isInteger(newValue)){
+                int bitLength = mBitLengthArys[mComboBoxBitData.indexOf(mComboBoxBit.getValue())];
+                mEditNum2.setText("" + Integer.parseInt(newValue));
+                mLabelCNum2.setText(NumberUtils.transBinNum(bitLength, newValue));
+                mLabelONum2.setText(NumberUtils.transComplementNum(bitLength, newValue));
+            }else {
+                mEditNum2.setText(oldValue);
+            }
+
+        });
     }
 
     public void buttonAction(ActionEvent event){
-        Log.d("点击！");
+
+        Log.d("" + mComboBoxBitData.indexOf(mComboBoxBit.getValue()));
     }
 
 }
